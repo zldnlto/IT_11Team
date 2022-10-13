40a70f3d93d327e5e6949fbabfa20894f669ed55
sewon kim <sewon0325@naver.com>
2022-10-14 00:28:00 +0900

    #20 3번문제

---
+++ JS100제/김세원/003.js
@@ -0,0 +1,14 @@
+// 문제3 : 변수의 타입
+// 다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+```
+ 1. undefined
+ 2. string
+ 3. number
+ 4. object
+ ```
+
+ //4번  object => JS에서는 array타입이 없고 object에 포함된다.
