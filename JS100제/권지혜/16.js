cb2b1743ea206ab684946368179eccda383cdd08
jihye.kwon <kwonjhjyj@naver.com>
2022-10-23 21:43:56 +0900

    #36 16번문제

---
+++ JS100제/권지혜/16.js
@@ -0,0 +1,7 @@
+/*# 문제16 : 로꾸거
+
+문장이 입력되면 거꾸로 출력하는 프로그램을 만들어 봅시다.
+
+답: const n = prompt('내용을 입력하세요');
+    const reverseSentence = n.split('').reverse().join('');
+    console.log(reverseSentence);
