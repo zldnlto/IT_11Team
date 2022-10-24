3927f537a41503c80c123c652d3dc48ed069ac90
hb_kim <binikim66@gmail.com>
2022-10-25 00:03:49 +0900

    #32 12번 문제

---
+++ JS100제/김현빈/012.js
@@ -0,0 +1,34 @@
+// 문제12 : 게임 캐릭터 클래스 만들기
+// 다음 소스코드에서 클래스를 작성하여 게임 캐릭터의 능력치와 '파이어볼'이 출력되게 만드시오.
+// 주어진 소스 코드를 수정해선 안됩니다.
+// 여기에 class를 작성하세요.
+
+// 1) 생성자 함수
+function Wizard(health, mana, armor) {
+  this.health = health;
+  this.mana = mana;
+  this.armor = armor;
+  this.attack = function () {
+    console.log("파이어볼");
+  };
+}
+
+// 2) 클래스
+class Wizard {
+  constructor(health, mana, armor) {
+    this.health = health;
+    this.mana = mana;
+    this.armor = armor;
+  }
+  attack() {
+    console.log("파이어볼");
+  }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+
+// 출력
+// 545 210 10
+// 파이어볼
