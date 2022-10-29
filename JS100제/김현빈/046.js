e5092d37ef33d44225b518792e8ab3d4fcad1418
hb_kim <binikim66@gmail.com>
2022-10-29 23:06:52 +0900

    #66 46버 문제

--- JS100제/김현빈/046.js
+++ JS100제/김현빈/046.js
@@ -11,6 +11,9 @@ function sum() {
   for (let i = 1; i <= 20; i++) {
     num += i;
   }
-  return num.split("").map(i => +i).reduce((a,b)=>a+b)
+  return num
+    .split("")
+    .map((i) => +i)
+    .reduce((a, b) => a + b);
 }
-function sum()
+sum();
