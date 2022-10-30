2e681df1eba837f831aaa9c114ab7a005e0c199a
hb_kim <binikim66@gmail.com>
2022-10-30 16:26:38 +0900

    #70 50번 문제

--- JS100제/김현빈/050.js
+++ JS100제/김현빈/050.js
@@ -10,7 +10,7 @@ function bubble(arr) {
   let result = arr.slice();
 
   for (let i = 0; i < result.length - 1; i++) {
-    for (let j=0; j < ) {
+    for (let j = 0; j < result.length - i; j++) {
       if (result[j] > result[j + 1]) {
         let value = result[j];
         result[j] = result[j + 1];
